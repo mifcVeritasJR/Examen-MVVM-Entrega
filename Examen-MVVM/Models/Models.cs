8597b52474b6ac6ac6fcfcb49aead3b9b5da8e1d
mifcVeritasJR <167486636+mifcVeritasJR@users.noreply.github.com>
2024-12-11 18:36:59 -0600

    Add project files.

---
+++ Examen-MVVM/Models/Models.cs
@@ -0,0 +1,14 @@
+﻿namespace Examen_MVVM.Models
+{
+    public class Producto
+    {
+        public decimal Precio { get; set; }
+    }
+
+    public class CalculoDescuento
+    {
+        public decimal Subtotal { get; set; }
+        public decimal Descuento { get; set; }
+        public decimal Total { get; set; }
+    }
+}
---
+++ Examen-MVVM/ViewModels/ViewModels.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Examen_MVVM.Models;
+
+namespace Examen_MVVM.ViewModels
+{
+    public class MainPageViewModel :
+}
