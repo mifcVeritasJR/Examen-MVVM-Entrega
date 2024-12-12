1ff68509690cba7551f01d3c505537aca4340670
mifcVeritasJR <167486636+mifcVeritasJR@users.noreply.github.com>
2024-12-11 18:42:35 -0600

    Creando viewmodel del proyecto donde se incluyen los 3 productos, subtotal, descuento y total

--- Examen-MVVM/ViewModels/ViewModels.cs
+++ Examen-MVVM/ViewModels/ViewModels.cs
@@ -4,5 +4,37 @@ using Examen_MVVM.Models;
 
 namespace Examen_MVVM.ViewModels
 {
-    public class MainPageViewModel :
+    public class MainPageViewModel : ObservableObject
+    {
+        private decimal _producto1;
+        private decimal _producto2;
+        private decimal _producto3;
+        private decimal _subtotal;
+        private decimal _descuento;
+        private decimal _total;
+
+        private decimal Producto1
+        {
+            get => _producto1;
+            set => SetProperty(ref _producto1, value);
+        }
+
+        public decimal Producto2
+        {
+            get => _producto2;
+            set => SetProperty(ref _producto2, value);
+        }
+
+        public decimal Producto3
+        {
+            get => _producto3;
+            set => SetProperty(ref _producto3, value);
+        }
+
+        public decimal Subtotal
+        {
+            get => _subtotal;
+            set => SetProperty(ref _subtotal, value);
+        }
+    }
 }
