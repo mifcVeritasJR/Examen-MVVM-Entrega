d477050d5076cf2d1528218c6b9be387bfd6498b
mifcVeritasJR <167486636+mifcVeritasJR@users.noreply.github.com>
2024-12-11 19:11:42 -0600

    editando el try catch que da error por variable declarada pero no utilizada

--- Examen-MVVM/MainPage.xaml.cs
+++ Examen-MVVM/MainPage.xaml.cs
@@ -1,25 +1,14 @@
-﻿namespace Examen_MVVM
+﻿using Examen_Mvvm.ViewModels;
+using Examen_MVVM.ViewModels;
+
+namespace Examen_Mvvm
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            BindingContext = new MainPageViewModel();
         }
     }
-
 }
